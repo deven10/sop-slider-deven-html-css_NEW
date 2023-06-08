7a03fa319325c4878dcef379ca5737ca7e82c95e
Deven Umrania <umraniadeven10@gmail.com>
2023-06-09 01:25:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const testiMonialContentsOne = document.querySelector(
+  ".testimonial-content-one"
+);
+const testiMonialContentsTwo = document.querySelector(
+  ".testimonial-content-two"
+);
+const testiMonialContentsThree = document.querySelector(
+  ".testimonial-content-three"
+);
+const testiMonialContentsFour = document.querySelector(
+  ".testimonial-content-four"
+);
+const testiMonialContentsFive = document.querySelector(
+  ".testimonial-content-five"
+);
